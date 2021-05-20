a120c620331bbdb9cf2c2f4b05db7ca21558fa10
MartinBarbieri <martinbarbieri17@gmail.com>
2021-05-20 17:12:54 -0300

    Modal para crear nueva categoria y la inserta en la base. Falta realizar las verificaciones (Que no se repita el color)

--- Login_CRUD/app/src/main/java/com/example/login_crud/TarjetasYCategoriasActivity.java
+++ Login_CRUD/app/src/main/java/com/example/login_crud/TarjetasYCategoriasActivity.java
@@ -1,19 +1,26 @@
 package com.example.login_crud;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.collection.ArraySet;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -23,45 +30,81 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TarjetasYCategoriasActivity extends AppCompatActivity {
+public class TarjetasYCategoriasActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Context context = this.getApplicationContext();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarjetas_y_categorias);
-        traerDatos();
+        Button aniadirCategoria = (Button) findViewById(R.id.aniadirCategoria);
+        aniadirCategoria.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                System.out.println("on click nueva categoria");
+                aniadirCategoria(v);
+            }
+        });
+        traerDatos(context);
+    }
 
-        }
+    public void aniadirCategoria(View view) {
 
+        LayoutInflater inflater = LayoutInflater.from(TarjetasYCategoriasActivity.this);
+        View dialog_layout = inflater.inflate(R.layout.activity_aniadir_categoria,null);
+        AlertDialog.Builder db = new AlertDialog.Builder(this);
+        db.setView(dialog_layout);
+        EditText nombre = dialog_layout.findViewById(R.id.nombreCategoria);;
+        EditText color = dialog_layout.findViewById(R.id.colorCategoria);
+        db.setTitle("Nueva categoria");
+        db.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int i) {
+                String nombreCategoria = nombre.getText().toString();
+                String colorCategoria = color.getText().toString();
+                System.out.println("nombre: "+ nombreCategoria);
+                insertarCategoria(nombreCategoria,colorCategoria);
 
-    private void traerDatos()  {
-        HashMap<Integer, Object> categorias = new HashMap<>();
-        HashMap<String, Object> categoria = new HashMap<>();
-        categoria.put("nombre","calle");
-        categoria.put("color","rosa");
-        categoria.put("cantidadTarjetas",4);
+            }
+        });
+        AlertDialog dialog = db.show();
+    }
+
+    private void insertarCategoria(String nombre, String color){
+        System.out.println("Inserto categoria");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        categorias.put(0,categoria);
-        Context context = this.getApplicationContext();
+        Map<String, Object> categoria = new HashMap<>();
+        categoria.put("color", color);
+        categoria.put("nombre", nombre);
+        categoria.put("tarjeta", new ArrayList<>());
+        db.collection("categorias")
+                .add(categoria)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
+    }
+
+    private void traerDatos(Context context)  {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("categorias").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        int indice = 0;
                         String TAG = "";
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 LinearLayout llBotonera = (LinearLayout) findViewById(R.id.llBotonera);
-                                //Creamos las propiedades de layout que tendrán los botones.
-                                //Son LinearLayout.LayoutParams porque los botones van a estar en un LinearLayout.
                                 LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT );
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                                System.out.println(document.getId() + " => " + document.getData());
                                 List<Map<String, String>> tarjetas = (List<Map<String, String>>) document.getData().get("tarjeta");
                                 Button button = new Button(context);
-                                //Asignamos propiedades de layout al boton
                                 button.setLayoutParams(lp);
-                                //Asignamos Texto al botón
                                 button.setText((String) document.getData().get("nombre") + " " + tarjetas.size());
-                                //Añadimos el botón a la botonera
                                 button.setBackgroundColor(939393);
                                 llBotonera.addView(button);
                             }
---
+++ Login_CRUD/app/src/main/java/com/example/login_crud/aniadirCategoria.java
@@ -0,0 +1,29 @@
+package com.example.login_crud;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class aniadirCategoria extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Prueba");
+//                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // FIRE ZE MISSILES!
+//                    }
+//                })
+//                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // User cancelled the dialog
+//                    }
+//                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
