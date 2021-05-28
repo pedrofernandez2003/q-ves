4bf4d4ada76bc1032d953659f54fd7c02d343952
Magali-Cristobo <alumno21.cristobo.magali@ipm.edu.ar>
2021-05-28 17:42:58 -0300

    agregamos una funcion que enciende el hotspot

--- Hotspot/app/src/main/java/com/example/hotspot/MainActivity.java
+++ Hotspot/app/src/main/java/com/example/hotspot/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.hotspot;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +26,8 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
@@ -26,6 +37,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "";
     Button botonServer, botonCliente, botonSend;
     TextView IPDispositivo;
     TextInputEditText inputIP, inputMensaje;
@@ -35,24 +47,33 @@ public class MainActivity extends AppCompatActivity {
     ClientClass clientClass;
     SendReceive sendReceive;
 
+    private WifiManager wifiManager;
+    WifiConfiguration currentConfig;
+    WifiManager.LocalOnlyHotspotReservation hotspotReservation;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IPDispositivo=findViewById(R.id.IPDispositivo);
-        inputIP=findViewById(R.id.inputIPServer);
-        inputMensaje=findViewById(R.id.inputMensaje);
-        botonServer=findViewById(R.id.botonServer);
-        botonSend=findViewById(R.id.botonSend);
-        botonCliente=findViewById(R.id.botonCliente);
-//        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-//        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager=(WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        currentConfig=new WifiConfiguration();
+        currentConfig.SSID="\"queves\"";
+        turnOnHotspot();
+        IPDispositivo = findViewById(R.id.IPDispositivo);
+        inputIP = findViewById(R.id.inputIPServer);
+        inputMensaje = findViewById(R.id.inputMensaje);
+        botonServer = findViewById(R.id.botonServer);
+        botonSend = findViewById(R.id.botonSend);
+        botonCliente = findViewById(R.id.botonCliente);
         IPDispositivo.setText(getIPAddress(true));
+
         botonServer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("tocaste server");
-                serverClass=new ServerClass();
+                serverClass = new ServerClass();
                 serverClass.start();
             }
         });
@@ -61,9 +82,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 System.out.println("tocaste cliente");
-                String ipServer=inputIP.getText().toString();
+                String ipServer = inputIP.getText().toString();
                 System.out.println(ipServer);
-                clientClass=new ClientClass(ipServer);
+                clientClass = new ClientClass(ipServer);
                 clientClass.start();
             }
         });
@@ -71,25 +92,26 @@ public class MainActivity extends AppCompatActivity {
         botonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String msg=inputMensaje.getText().toString();
-                byte[] bytesMsg=msg.getBytes();
-                for(int i=0;i<bytesMsg.length;i++){
+                String msg = inputMensaje.getText().toString();
+                byte[] bytesMsg = msg.getBytes();
+                for (int i = 0; i < bytesMsg.length; i++) {
                     System.out.println(bytesMsg[i]);
                 }
-                System.out.println("tocaste send 1"+ msg);
-                Write escribir=new Write();
+                System.out.println("tocaste send 1" + msg);
+                Write escribir = new Write();
                 escribir.execute(bytesMsg);
 //                sendReceive.write("hola".getBytes());
             }
         });
     }
-    Handler handler=new Handler(new Handler.Callback() {
+
+    Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(@NonNull Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case MESSAGE_READ:
-                    byte[] readBuff= (byte[]) msg.obj;
-                    String tempMsg=new String(readBuff,0,msg.arg1);
+                    byte[] readBuff = (byte[]) msg.obj;
+                    String tempMsg = new String(readBuff, 0, msg.arg1);
                     Toast.makeText(getApplicationContext(), tempMsg, Toast.LENGTH_SHORT).show();
                     break;
             }
@@ -97,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
-    public class ServerClass extends Thread{
+    public class ServerClass extends Thread {
         Socket socket;
         ServerSocket serverSocket;
 
@@ -105,9 +127,9 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             try {
                 System.out.println("entre al run server");
-                serverSocket=new ServerSocket(7028);
-                socket=serverSocket.accept();
-                sendReceive=new SendReceive(socket);
+                serverSocket = new ServerSocket(7028);
+                socket = serverSocket.accept();
+                sendReceive = new SendReceive(socket);
                 sendReceive.start();
             } catch (Exception e) {
                 System.out.println("entre al catch");
@@ -116,18 +138,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class SendReceive extends Thread{
+    private class SendReceive extends Thread {
         private Socket socket;
         private InputStream inputStream;
         private OutputStream outputStream;
 
-        public SendReceive(Socket skt){
+        public SendReceive(Socket skt) {
             System.out.println("entre al constructor");
-            socket=skt;
+            socket = skt;
             try {
                 System.out.println("se construyo el sendReceive");
-                inputStream=socket.getInputStream();
-                outputStream=socket.getOutputStream();
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
             } catch (IOException e) {
                 System.out.println("entre al catch");
                 e.printStackTrace();
@@ -136,20 +158,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            byte[] buffer= new byte[1024];
+            byte[] buffer = new byte[1024];
             int bytes;
-            while (socket!=null){
+            while (socket != null) {
                 try {
-                    bytes=inputStream.read(buffer);
-                    if (bytes>0){
-                        handler.obtainMessage(MESSAGE_READ,bytes,-1,buffer).sendToTarget();
+                    bytes = inputStream.read(buffer);
+                    if (bytes > 0) {
+                        handler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget();
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        public void write(byte[] bytes){
+
+        public void write(byte[] bytes) {
             try {
                 outputStream.write(bytes);
             } catch (IOException e) {
@@ -158,27 +181,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class ClientClass extends Thread{
+    public class ClientClass extends Thread {
         Socket socket;
         String hostAdd;
-        public ClientClass(String hostAddress){
-            hostAdd=hostAddress;
-            socket=new Socket();
+
+        public ClientClass(String hostAddress) {
+            hostAdd = hostAddress;
+            socket = new Socket();
         }
 
         @Override
         public void run() {
             try {
                 System.out.println("entre al run client");
-                socket.connect(new InetSocketAddress(hostAdd,7028),5000);
-                sendReceive=new SendReceive(socket);
+                socket.connect(new InetSocketAddress(hostAdd, 7028), 5000);
+                sendReceive = new SendReceive(socket);
                 sendReceive.start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    public class Write extends AsyncTask{
+
+    public class Write extends AsyncTask {
         @Override
         protected Object doInBackground(Object[] objects) {
             try {
@@ -199,7 +224,7 @@ public class MainActivity extends AppCompatActivity {
                     if (!addr.isLoopbackAddress()) {
                         String sAddr = addr.getHostAddress();
                         //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
-                        boolean isIPv4 = sAddr.indexOf(':')<0;
+                        boolean isIPv4 = sAddr.indexOf(':') < 0;
 
                         if (useIPv4) {
                             if (isIPv4)
@@ -207,13 +232,50 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             if (!isIPv4) {
                                 int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
-                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                                return delim < 0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
                             }
                         }
                     }
                 }
             }
-        } catch (Exception ex) { } // for now eat exceptions
+        } catch (Exception ex) {
+        } // for now eat exceptions
         return "";
     }
+
+    private void hotspotDetailsDialog() {
+        Log.v(TAG, this.getApplicationContext().getString(R.string.hotspot_details_message) + "\n" + this.getApplicationContext().getString(
+                R.string.hotspot_ssid_label) + " " + currentConfig.SSID + "\n" + this.getApplicationContext().getString(
+                R.string.hotspot_pass_label) + " " + currentConfig.preSharedKey);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void turnOnHotspot() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            wifiManager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
+                @Override
+                public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
+                    super.onStarted(reservation);
+                    hotspotReservation = reservation;
+                    currentConfig = hotspotReservation.getWifiConfiguration();
+                    Log.v("DANG", "THE PASSWORD IS: " + currentConfig.preSharedKey + " \n SSID is : " + currentConfig.SSID);
+                    hotspotDetailsDialog();
+                }
+
+                @Override
+                public void onStopped() {
+                    super.onStopped();
+                    Log.v("DANG", "Local Hotspot Stopped");
+                }
+
+                @Override
+                public void onFailed(int reason) {
+                    super.onFailed(reason);
+                    Log.v("DANG", "Local Hotspot failed to start");
+                }
+            }, new Handler());
+        }
+    }
+
 }
