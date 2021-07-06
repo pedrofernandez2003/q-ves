253de963c6bff32cc4fda50311b20d4e2feeeae7
MartinBarbieri <martinbarbieri17@gmail.com>
2021-07-06 15:11:48 -0300

    Menu desplegable categorias

--- Login_CRUD/app/src/main/java/com/example/login_crud/CrearJuegoActivity.java
+++ Login_CRUD/app/src/main/java/com/example/login_crud/CrearJuegoActivity.java
@@ -2,41 +2,104 @@ package com.example.login_crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.Listeners.onTraerDatosListener;
+import com.example.Objetos.Categoria;
+
+import java.util.ArrayList;
 
 public class CrearJuegoActivity extends AppCompatActivity  {
-@Override
+    Button mOrder;
+    TextView mItemSelected;
+    ArrayList<String> listItems;
+    boolean[] checkedItems;
+    ArrayList<Integer> mUserItems = new ArrayList<>();
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_administrador);
-//         Button crearJuego = (Button) findViewById(R.id.crearJuego);
-//         Button iniciarJuego = (Button) findViewById(R.id.iniciarJuego);
-//         Button administrarElementos = (Button) findViewById(R.id.adminElementos);
-//         crearJuego.setOnClickListener(new View.OnClickListener() {
-//             @Override
-//             public void onClick(View v) {
-//                 Intent i = new Intent(CrearJuegoActivity.this, CrearJuegoActivity.class);
-//                 startActivity(i);
-//             }
-//         });
-//         iniciarJuego.setOnClickListener(new View.OnClickListener() {
-//                     @Override
-//                     public void onClick(View v) {
-//                         Intent i = new Intent(CrearJuegoActivity.this, IniciarJuegoActivity.class);
-//                         startActivity(i);
-//                     }
-//          });
-//          administrarElementos.setOnClickListener(new View.OnClickListener() {
-//                      @Override
-//                      public void onClick(View v) {
-//                          Intent i = new Intent(CrearJuegoActivity.this, AdminElementosActivity.class);
-//                          startActivity(i);
-//                      }
-//           });
+        setContentView(R.layout.activity_crear_juego);
 
-    }
+    mOrder = (Button) findViewById(R.id.btnOrder);
+    ArrayList<String> nombresCategoria = new ArrayList<>();
+    mItemSelected = (TextView) findViewById(R.id.tvItemSelected);
+    DataManagerCategoria.traerCategorias(new onTraerDatosListener() {
+        @Override
+        public void traerDatos(ArrayList<Object> datos) {
+            checkedItems = new boolean[datos.size()];
+            for (Object dato: datos) {
+                Categoria categoria= (Categoria) dato;
+                nombresCategoria.add(categoria.getNombre());
+            }
+            listItems = nombresCategoria;
+        }
+    });
 
+
+    mOrder.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(CrearJuegoActivity.this);
+            mBuilder.setTitle(R.string.dialog_title);
+            mBuilder.setMultiChoiceItems(listItems.toArray(new String[0]), checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+                    if(isChecked){
+                        mUserItems.add(position);
+                    }else{
+                        mUserItems.remove((Integer.valueOf(position)));
+                    }
+                }
+            });
+
+            mBuilder.setCancelable(false);
+            mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    String item = "";
+                    for (int i = 0; i < mUserItems.size(); i++) {
+                        item = item + listItems.get(mUserItems.get(i));
+                        if (i != mUserItems.size() - 1) {
+                            item = item + ", ";
+                        }
+                    }
+                    mItemSelected.setText(item);
+                }
+            });
+
+            mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+
+            mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    for (int i = 0; i < checkedItems.length; i++) {
+                        checkedItems[i] = false;
+                        mUserItems.clear();
+                        mItemSelected.setText("");
+                    }
+                }
+            });
+
+            AlertDialog mDialog = mBuilder.create();
+            mDialog.show();
+        }
+    });
 }
+}
+
+
+
