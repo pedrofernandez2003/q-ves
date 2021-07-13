5daf096a641e3540f9109ea16660e31aceceeb9e
MartinBarbieri <martinbarbieri17@gmail.com>
2021-07-13 11:54:24 -0300

    Hago una prueba para que me traiga las imagenes. Revisar por qué no funciona. Fijarse traer categorias de crear juego...está vacio

---
+++ Login_CRUD/app/src/main/java/com/example/Objetos/ViewHolder.java
@@ -0,0 +1,32 @@
+package com.example.Objetos;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.login_crud.R;
+import com.squareup.picasso.Picasso;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    View view;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        view = itemView;
+    }
+
+    public void setDetails(Context context, String image){
+        ImageView mImagetv = view.findViewById(R.id.rImageView);
+
+        Picasso.get().load(image).into(mImagetv);
+
+
+
+    }
+}
--- Login_CRUD/app/src/main/java/com/example/login_crud/CrearJuegoActivity.java
+++ Login_CRUD/app/src/main/java/com/example/login_crud/CrearJuegoActivity.java
@@ -1,6 +1,9 @@
 package com.example.login_crud;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -8,6 +11,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +20,12 @@ import android.widget.TextView;
 
 import com.example.Listeners.onTraerDatosListener;
 import com.example.Objetos.Categoria;
+import com.example.Objetos.Member;
+import com.example.Objetos.ViewHolder;
+//import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 import java.util.ArrayList;
 
@@ -25,90 +35,118 @@ public class CrearJuegoActivity extends AppCompatActivity  {
     ArrayList<String> listItems;
     boolean[] checkedItems;
     ArrayList<Integer> mUserItems = new ArrayList<>();
-    ImageView mImageView;
-//    ProgressDialog
+    RecyclerView mRecyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_juego);
-
-        
-    mOrder = (Button) findViewById(R.id.btnOrder);
-    ArrayList<String> nombresCategoria = new ArrayList<>();
-    mItemSelected = (TextView) findViewById(R.id.tvItemSelected);
-    DataManagerCategoria.traerCategorias(new onTraerDatosListener() {
-        @Override
-        public void traerDatos(ArrayList<Object> datos) {
-            checkedItems = new boolean[datos.size()];
-            for (Object dato: datos) {
-                Categoria categoria= (Categoria) dato;
-                nombresCategoria.add(categoria.getNombre());
-            }
-            listItems = nombresCategoria;
-        }
-    });
-
-
-    mOrder.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            AlertDialog.Builder mBuilder = new AlertDialog.Builder(CrearJuegoActivity.this);
-            mBuilder.setTitle(R.string.dialog_title);
-            mBuilder.setMultiChoiceItems(listItems.toArray(new String[0]), checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
-                    if(isChecked){
-                        mUserItems.add(position);
-                    }else{
-                        mUserItems.remove((Integer.valueOf(position)));
-                    }
+        mOrder = (Button) findViewById(R.id.btnOrder);
+        ArrayList<String> nombresCategoria = new ArrayList<>();
+        mItemSelected = (TextView) findViewById(R.id.tvItemSelected);
+//        mRecyclerView = findViewById(R.id.recyclerview);
+//        mRecyclerView.setHasFixedSize(true);
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        firebaseDatabase = FirebaseDatabase.getInstance();
+//        reference = firebaseDatabase.getReference("Data");
+        DataManagerCategoria.traerCategorias(new onTraerDatosListener() {
+            @Override
+            public void traerDatos(ArrayList<Object> datos) {
+                checkedItems = new boolean[datos.size()];
+                for (Object dato: datos) {
+                    Categoria categoria= (Categoria) dato;
+                    nombresCategoria.add(categoria.getNombre());
                 }
-            });
+                listItems = nombresCategoria;
+            }
+        });
 
-            mBuilder.setCancelable(false);
-            mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int which) {
-                    String item = "";
-                    for (int i = 0; i < mUserItems.size(); i++) {
-                        item = item + listItems.get(mUserItems.get(i));
-                        if (i != mUserItems.size() - 1) {
-                            item = item + ", ";
+//        nombresCategoria.add("verde");
+//        nombresCategoria.add("amarillo");
+//        nombresCategoria.add("azul");
+//        listItems = nombresCategoria;
+
+
+        mOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(CrearJuegoActivity.this);
+                mBuilder.setTitle(R.string.dialog_title);
+                mBuilder.setMultiChoiceItems(listItems.toArray(new String[0]), checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+                        if(isChecked){
+                            mUserItems.add(position);
+                        }else{
+                            mUserItems.remove((Integer.valueOf(position)));
                         }
                     }
-                    mItemSelected.setText(item);
-                }
-            });
+                });
 
-            mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
-                }
-            });
-
-            mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int which) {
-                    for (int i = 0; i < checkedItems.length; i++) {
-                        checkedItems[i] = false;
-                        mUserItems.clear();
-                        mItemSelected.setText("");
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < mUserItems.size(); i++) {
+                            item = item + listItems.get(mUserItems.get(i));
+                            if (i != mUserItems.size() - 1) {
+                                item = item + ", ";
+                            }
+                        }
+                        mItemSelected.setText(item);
                     }
-                }
-            });
+                });
 
-            AlertDialog mDialog = mBuilder.create();
-            mDialog.show();
-        }
-    });
+                mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                            mUserItems.clear();
+                            mItemSelected.setText("");
+                        }
+                    }
+                });
+
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
 
 
 
 }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        FirebaseRecyclerAdapter<Member, ViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Member, ViewHolder>(Member.class,R.layout.image,ViewHolder.class,reference) {
+//            @Override
+//            protected void populateViewHolder(ViewHolder viewHolder, Member member, int i) {
+//
+//                viewHolder.setDetails(getApplicationContext(),member.getImage());
+//            }
+//        };
+//
+//        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+//        }
+
+
+
 }
+
 
 
 
