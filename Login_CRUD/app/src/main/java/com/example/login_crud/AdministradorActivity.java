95ef05e88be0b45d9b7273ae6083227445f7f92f
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2021-07-19 16:08:14 -0300

    Update AdministradorActivity.java

--- Login_CRUD/app/src/main/java/com/example/login_crud/AdministradorActivity.java
+++ Login_CRUD/app/src/main/java/com/example/login_crud/AdministradorActivity.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.Listeners.onTraerDatosListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 
 public class AdministradorActivity extends AppCompatActivity {
@@ -26,6 +29,9 @@ public class AdministradorActivity extends AppCompatActivity {
         mAuth= FirebaseAuth.getInstance();
         FirebaseUser currentUser= mAuth.getCurrentUser();
         Toast.makeText(AdministradorActivity.this,currentUser.getDisplayName(),Toast.LENGTH_SHORT).show();
+
+
+
         crearJuego = (Button) findViewById(R.id.crearJuego);
         iniciarJuego = (Button) findViewById(R.id.iniciarJuego);
         administrarElementos = (Button) findViewById(R.id.adminElementos);
