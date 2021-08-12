289ecef61be576d1eca021263dbbcb366db864fa
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2021-08-12 16:25:28 -0300

    Arranco a laburar en el tablero

---
+++ Hotspot/app/src/main/java/com/example/hotspot/CrearTablero.java
@@ -0,0 +1,13 @@
+package com.example.hotspot;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CrearTablero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablero);
+    }
+}
