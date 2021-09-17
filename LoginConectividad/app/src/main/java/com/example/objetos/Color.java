47e120a3c7391918a0e423daa3ad5c95bf388ba3
MartinBarbieri <martinbarbieri17@gmail.com>
2021-09-17 10:36:43 -0300

    Arreglo el xml de crear usuario

--- LoginConectividad/app/src/main/java/com/example/objetos/Color.java
+++ LoginConectividad/app/src/main/java/com/example/objetos/Color.java
@@ -2,9 +2,9 @@ package com.example.objetos;
 
 
 public  enum Color {
-    AMARILLO(0xffffff00), VERDE(0xff008000 ), CELESTE(0xff13c8f5), TURQUESA(0xff67e2aa),
+    AMARILLO(0xfffed713), VERDE(0xff008000 ), CELESTE(0xff13c8f5), TURQUESA(0xff67e2aa),
     AZUL(0xff0f3bee),  VIOLETA(0xff3D00FF),GRIS(0xff808080), ROJO(0xffC21807), ROSA(0xffE63C8E), NARANJA(0xffF04A00),
-    NEGRO(0xff1b1bbf);
+    NEGRO(0xff1b1bbf), AZULOSCURO(0xff262262);
 
 //    private String nombre;
     private final int codigo;
